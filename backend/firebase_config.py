0d98450718608025a8574988ad9a3bc3170f7950
InSange <ehfrur888@naver.com>
2024-11-23 18:00:34 +0900

    fix : 환경변수 설정 변경
    #3

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,8 +1,27 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, firestore, db
+from dotenv import load_dotenv
 
-# Firebase 서비스 계정 키 파일 경로
-cred = credentials.Certificate("pigbrothers-b3b15-firebase-adminsdk-btdv8-6012fc4f1b.json")
+load_dotenv()
+
+firebase_config = {
+    "type": os.environ.get("TYPE"),
+    "project_id": os.environ.get("PROJECT_ID"),
+    "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("PRIVATE_KEY"),
+    "client_email": os.environ.get("CLIENT_EMAIL"),
+    "client_id": os.environ.get("CLIENT_ID"),
+    "auth_uri": os.environ.get("AUTH_URI"),
+    "token_uri": os.environ.get("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("CLIENT_X509_CERT_URL"),
+    "universe_domain": os.environ.get("UNIVERSE_DOMAIN")
+}
+
+# 자격증명을 생성하여 Firebase 초기화
+cred = credentials.Certificate(firebase_config)
 
 # Realtime Database 초기화
 firebase_admin.initialize_app(cred, {
