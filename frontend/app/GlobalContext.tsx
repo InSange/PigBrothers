6bc73e445e096bdeafe1f4d3bdc828b8963e79c1
chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2024-11-30 12:29:57 +0900

    design : home 디자인

---
+++ frontend/app/GlobalContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, PropsWithChildren, useMemo, useState } from 'react';
+
+type ContextType = {
+  name?: string;
+  handleChangeName: (name: string) => void;
+};
+export const GlobalContext = createContext<ContextType>({
+  handleChangeName: (_: string) => {},
+});
+
+export const GlobalContextProvider = ({ children }: PropsWithChildren) => {
+  const [name, setName] = useState<ContextType['name']>();
+
+  const handleChangeName = (name: string) => {
+    setName(name);
+  };
+  const contextValue = useMemo(() => ({ name, handleChangeName }), [name]);
+
+  return (
+    <GlobalContext.Provider value={contextValue}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
