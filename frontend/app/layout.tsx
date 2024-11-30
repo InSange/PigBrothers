38ce3458959f2f11844712347db8cd94c67e6b69
chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2024-11-30 12:29:37 +0900

    chore : react-scan 세팅

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,10 +1,13 @@
-import { ReactNode } from 'react';
-import StyledComponentsRegistry from '@/lib/styled-components-registry';
 import ClientLayout from '@/lib/client-layout';
+import StyledComponentsRegistry from '@/lib/styled-components-registry';
+import { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en'>
+    <html lang='ko'>
+      <head>
+        <script src='https://unpkg.com/react-scan/dist/auto.global.js' async />
+      </head>
       <body>
         <StyledComponentsRegistry>
           <ClientLayout>{children}</ClientLayout>
